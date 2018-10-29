d11a1a99bf88bb28a34c00ab733976d5e047f75b
Tania Whiting <tanwhiting@aol.com>
2018-10-29 21:19:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,16 @@ class Song
   def self.all
     @@all
   end
+  
+  def self.create
+    def initialize(name)
+      name = Song.new
+      @@all << name
 
   def save
     self.class.all << self
   end
+  
+
 
 end
